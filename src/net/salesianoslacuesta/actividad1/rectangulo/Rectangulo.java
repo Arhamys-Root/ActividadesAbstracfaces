68c5658da239bf0ee73d58c93c55b65da8608360
Arhamys Root <arhamys14@gmail.com>
2026-04-24 18:29:39 +0100

    agregamos clase rectangulo

---
+++ src/net/salesianoslacuesta/actividad1/rectangulo/Rectangulo.java
@@ -0,0 +1,32 @@
+package net.salesianoslacuesta.actividad1.rectangulo;
+
+import net.salesianoslacuesta.actividad1.calculoGeometricoBidimensional.CalculoGeometricoBidimensional;
+
+public class Rectangulo implements CalculoGeometricoBidimensional {
+
+    private double base;
+    private double altura;
+
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    @Override
+    public double calcularArea() {
+        return base * altura;
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        return 2 * (base + altura);
+    }
+}
