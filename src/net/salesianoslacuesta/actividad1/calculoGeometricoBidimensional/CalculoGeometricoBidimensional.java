5ac9a52b42daa3b9b39d0b3c056ad2d868a22f01
Arhamys Root <arhamys14@gmail.com>
2026-04-24 18:23:46 +0100

    añadimos la interface calculo

---
+++ src/net/salesianoslacuesta/actividad1/calculoGeometricoBidimensional/CalculoGeometricoBidimensional.java
@@ -0,0 +1,6 @@
+package net.salesianoslacuesta.actividad1.calculoGeometricoBidimensional;
+
+public interface CalculoGeometricoBidimensional {
+    double calcularArea();
+    double calcularPerimetro();
+}
