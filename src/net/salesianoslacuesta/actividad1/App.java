c5fcf2e5ce865c4eb04c0f24a4ef9e69b9122d50
Arhamys Root <arhamys14@gmail.com>
2026-04-24 18:13:07 +0100

    Establecemos la base de las actividades

--- src/net/salesianoslacuesta/actividad1/App.java
+++ src/net/salesianoslacuesta/actividad1/App.java
@@ -1,3 +1,5 @@
+package net.salesianoslacuesta.actividad1;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
---
+++ src/net/salesianoslacuesta/actividad2/App.java
@@ -0,0 +1,5 @@
+package net.salesianoslacuesta.actividad2;
+
+public class App {
+    
+}
